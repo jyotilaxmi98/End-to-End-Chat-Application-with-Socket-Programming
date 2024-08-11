4d45d6f4d6aa8eb6a00dc3ab3be9c0172569c97f
Jyotilaxmi Masahari <jyotilaxmimasahari241@gmail.com>
2024-08-11 10:00:04 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,10 +13,15 @@ int main(){
 
     if(!Initialise()){
         cout<<"Winsock Initisalisation failed"<<endl;
+        return 1;
     }
 
     cout<<"Server Program"<< endl;
     SOCKET listenSocket = socket(AF_INET,SOCK_STREAM,0);
+    if(listenSocket == INVALID_SOCKET){
+        cout<<"socket creation failed"<< endl;
+        return 1; //since socket programming failed,no point getting further so return with 1
+    }
 
     WSACleanup();
 
