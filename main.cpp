67b488f80b2ccf73086b6c6418216b2d04b08867
Jyotilaxmi Masahari <jyotilaxmimasahari241@gmail.com>
2024-08-11 09:46:33 +0530

    Created main.cpp
    server

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<WinSock2.h>
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib")
+
+bool Initialise(){
+    WSADATA data;
+    return WSAStartup(MAKEWORD(2,2), &data) == 0;
+}
+
+int main(){
+
+    if(!Initialise()){
+        cout<<"Winsock Initisalisation failed"<<endl;
+    }
+
+    cout<<"Server Program"<< endl;
+
+    WSACleanup();
+
+    return 0;
+}
